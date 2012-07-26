4e6c22e3d5a474c37d192158cd72fc9926ae3b83
psupsuoooo@gmail.com <psupsuoooo@gmail.com@d802b3e9-e0e7-d424-03f8-3e06a82a63a6>
2012-07-26 09:27:28 +0000

    new docs.

--- trunk/src/graph/Euler.java
+++ trunk/src/graph/Euler.java
@@ -8,6 +8,8 @@ import java.util.LinkedList;
  * http://blog.163.com/zhoumhan_0351/blog/static/39954227200982051154725/
  * http://www.cppblog.com/RyanWang/archive/2009/02/04/73021.html
  * http://www.cnblogs.com/buptLizer/archive/2012/04/15/2450297.html
+ * http://hi.baidu.com/ybq0460/blog/item/9f5e2272a445a7ea0bd18725.html
+ * http://ideone.com/3lfbC
  */
 
 public class Euler
