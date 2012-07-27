c015ee35520d9f65be225f8bdb7194df22cba980
psupsuoooo@gmail.com <psupsuoooo@gmail.com@d802b3e9-e0e7-d424-03f8-3e06a82a63a6>
2012-07-27 15:12:25 +0000

    1. reference 2. eular

--- src/TestCircularSortedArraySearch.java
+++ src/TestCircularSortedArraySearch.java
@@ -71,6 +71,49 @@ public class TestCircularSortedArraySearch
 		System.out.println(circularArraySearch(a, a.length, 1));
 		System.out.println(circularArraySearch(a, a.length, 3));
 		System.out.println(circularArraySearch(a, a.length, 5));
+		System.out.println(Search(a, a.length, 3));
 	}
+	
+	public static int Search(int A[],int n, int num)  
+	{  
+	    int left = 0 ;  
+	    int right = n - 1 ;  
+	    int mid = 0 ;  
+	    int pos = -1 ;    //返回-1，表示查找失败  
+	  
+	    while(left <= right)  
+	    {  
+	        mid = (left + right)/2 ;  
+	  
+	        if (A[mid] == num)  
+	        {     
+	            pos = mid ;   
+	            break;       
+	        }  
+	        if (A[left] <= A[mid])    //前半部分是严格递增的，后半部分是一个更小的循环递增数组  
+	        {  
+	            if(A[left] <= num && num  < A[mid] )  
+	            {  
+	                right = mid - 1 ;  
+	            }  
+	            else  
+	            {  
+	                left = mid + 1 ;  
+	            }   
+	        }  
+	        else    //后半部分是严格递增的，前半部分是一个更小的循环递增数组  
+	        {  
+	            if ( A[mid] < num && num <= A[right])  
+	            {  
+	                left = mid + 1 ;  
+	            }  
+	            else  
+	            {  
+	                right = mid - 1 ;  
+	            }  
+	        }  
+	    }  
+	    return pos;  
+	}  
 
 }
