36683b8df07632e6908eacb6e659081c74ca9433
psupsuoooo@gmail.com <psupsuoooo@gmail.com@d802b3e9-e0e7-d424-03f8-3e06a82a63a6>
2012-08-07 13:57:59 +0000

    Stoer_Wagner min cut and docs

--- src/graph/MinCut.java
+++ src/graph/MinCut.java
@@ -1,22 +1,5 @@
 package graph;
 
-import java.util.ArrayList;
-
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-//http://blog.sina.com.cn/s/blog_64018c2501011dd7.html
-//可以用最小切割最大流定理：
-//1.min=MAXINT,确定一个源点 为什么不枚举源点了呢？？？
-//2.枚举汇点
-//3.计算最大流，并确定当前源汇的最小割集，若比min小更新min
-//4.转到2直到枚举完毕
-//5.min即为所求输出min
-//    不难看出复杂度很高：枚举汇点要O(n)，最短增广路最大流算法求最大流是O((n^2)m)复杂度，在复杂网络中O(m)=O(n^2)，算法总复杂度就是O(n^5)；哪怕采用最高标号预进流算法求最大流O((n^2)(m^0.5))，算法总复杂度也要O(n^4)
-//    所以用网络流算法求解最小割集复杂度不会低于O(n^4)。
-    
 //1. 算法在做"最大生成树"时更新的不是普通意义上的最大边, 而是与之相连的边的权值和, 当所有边都是单位权值时就是累计度.
 //2. "最后进入A的两点记为s和t", 网上对s有两种解释, 一是在t之前一个加进去的点, 二是t的前趋节点, 也就是最后选择的那条边的另一端. 正解是第一种!
 //3. 对于稠密图, 比如这题, 我用堆, 映射二分堆, 或者STL的优先队列都会TLE, 还不如老老实实O(n^3).
@@ -46,60 +29,80 @@ import java.util.Map;
 //2. 选取 V - A中的 w(A, x)最大的点 x加入集合 A  
 //3. 若|A|=|V|，结束 
 //令倒数第二个加入 A的点为 s，最后一个加入 A的点为 t，则s-t 最小割为 w(At, t)
+
+//http://acm.nudt.edu.cn/~twcourse/Cut.html
+//详细看PPT与PDF
+//详见PPT 自己描述 每次求出一个 任意两点的s-t最小割,然后再把s,t合并成一个点,用好contract定理,再迭代求余小的点的任意s-t割,比较最小割值
 public class MinCut
 {
-	public static Map<Ve, Integer>		vertexMap	= new HashMap<Ve, Integer>();
-	public static List<E>				EdgeQueue	= new ArrayList<E>();
-	public static List<E>				EdgeList	= new ArrayList<E>();
-	public static int					MAX			= Integer.MIN_VALUE;
-	public static int					edgeCnt;
-	public static int					vertexCnt	= edgeCnt = 0;					// 点数，边数
+	public static int		edgeCnt;
+	public static int		vertexCnt	= edgeCnt = 0;	// 点数，边数
 
-	public static int					MAXN		= 10;
-	public static int[][]				capacity;
-	public static int[][]				flow;
-	public static int[]					minCapacity;
-	public static int[]					father;
-	public static int					maxFlow;
-	public static boolean[]				visit;
-	public static int[]					gap;
-	public static int[]					nextV;
-	public static int[]					H;											// H==Height
-	public static int[]					E;											// E=Excess
-	public static int[]					weight;
-	public static boolean[]				combine;
-	public static int[][]				map;
-	public static LinkedList<Integer>[]	activeHLPP;
+	public static int		MAXN		= 10;
+	public static boolean[]	visit;
+	public static int[]		weight;
+	public static boolean[]	combine;
+	public static int[][]	map;
+
+	public static int		s, t, minCut;
+	public static int		S, T;						// Global S,T
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args)
 	{
-		// int a, b, c, M;
-		// while(scanf("%d%d", &N, &M) != EOF){
-		// memset(map, 0, sizeof(map));
-		// while(M--){
-		// scanf("%d%d%d", &a, &b, &c);
-		// map[a][b] += c;
-		// map[b][a] += c;
+		edgeCnt = 4;
+		vertexCnt = 4;
+		combine = new boolean[vertexCnt + 1];
+		map = new int[vertexCnt + 1][vertexCnt + 1];
+		input();
+		System.out.println("min cut:" + Stoer_Wagner());
+		System.out.println("S===" + S);
+		System.out.println("T===" + T);
+	}
+
+	public static void input()
+	{
+		addEdge(1, 2, 1);
+		addEdge(2, 3, 3);
+		addEdge(4, 1, 2);
+		addEdge(3, 4, 1);
+		// int count = 0;
+		// for (int i = 1; i <= edgeCnt; i++) {
+		// int a = new Random().nextInt(vertexCnt - 1) + 1;// 1-n
+		// int b = new Random().nextInt(vertexCnt - 1) + 1;// 1-n
+		// int weight = new Random().nextInt(vertexCnt - 1) + 1;
+		// if (a != b) {
+		// if (map[a][b] == 0 || map[b][a] == 0) {
+		// addEdge(a, b, weight);
+		// count++;
 		// }
-		// printf("%d\n", Stoer_Wagner());
 		// }
+		// }
+		// // 去掉了重复了的edge
+		// edgeCnt = count;
+
+	}
+
+	public static void addEdge(int a, int b, int weight)
+	{
+		map[a][b] = weight;
+		map[b][a] = weight;
 	}
 
 	/* pku 2914 */
 
-	public static int	S, T, minCut, N;
-
+	// O(e + vlog(v)) E条边的InCreaseKey,V次的ExtraMax 用Fibo
+	//
 	public static void maximumAdjacencySearch()
 	{
 		int i, j, x = 0;
 		visit = new boolean[vertexCnt + 1];
 		weight = new int[vertexCnt + 1];
-		S = T = -1;
+		s = t = -1;
 		for (i = 1; i <= vertexCnt; i++) {
-			MAX = Integer.MIN_VALUE;
+			int MAX = Integer.MIN_VALUE;
 			// 找出一個尚未加入A當中、且w(A, x)最大的x點。
 			for (j = 1; j <= vertexCnt; j++) {
 				if (!combine[j] && !visit[j] && weight[j] > MAX) {
@@ -107,13 +110,17 @@ public class MinCut
 					MAX = weight[j];
 				}
 			}
-			if (T == x) {
+			if (t == x) {
+				System.out.println("s===" + s);
+				System.out.println("t===" + t);
+				System.out.println("minCut===" + minCut);
 				return;
 			}
 			// 不斷紀錄目前的Cut位置
-			S = T;
-			T = x;
+			s = t;
+			t = x;
 			minCut = MAX;
+
 			visit[x] = true; // 加入x点到A集合
 			// 加入a點到A集合後，更新w(A, x)的值。
 			for (j = 1; j <= vertexCnt; j++) {
@@ -122,31 +129,56 @@ public class MinCut
 				}
 			}
 		}
+		System.out.println("s===" + s);
+		System.out.println("t===" + t);
+		System.out.println("minCut===" + minCut);
 	}
 
+	// O(VE + V^2lg(V))
+	// V*O(e + vlog(v)) 采用F堆 每次E条边的INCREASE O(1),V次的EXTRACTMAX O(lgV)
+	// 如果不用是O(V*(E+V^2)) 共O(V^3) 这个是一般比较
+	// 最大堆就是O(V*(Elg(E)+Vlg(V))
 	public static int Stoer_Wagner()
 	{
 		int i, j;
 		combine = new boolean[vertexCnt + 1];
-		int ans = MAX;
-		for (i = 1; i <= vertexCnt; i++) {
+		int ans = Integer.MAX_VALUE;
+		for (i = 1; i <= vertexCnt - 1; i++) {
+			printMap(map);
 			maximumAdjacencySearch();
-			// s點和t點在Cut不同側
 			if (minCut < ans) {
+
+				T = t;
+				S = s;
 				ans = minCut;
 			}
 			if (ans == 0) {
 				return 0;
 			}
-			// s點和t點在Cut同側
-			combine[T] = true;// 把t點標記為被合併過了，變成不存在。
-			for (j = 0; j < N; j++) {
+			// contract(s, t);
+			combine[s] = true;// 把t點標記為被合併過了，變成不存在。
+			// 合并S和T都是等效的,
+			// 用这种方法不能求Global S,T 因为合并的点,并不能分出来,只能求minCut
+			for (j = 1; j <= vertexCnt; j++) {
 				if (!combine[j]) {
-					map[S][j] += map[T][j];
-					map[j][S] += map[j][T];
+					map[t][j] += map[s][j];
+					map[j][t] += map[j][s];
+					// map[s][j] += map[t][j];
+					// map[j][s] += map[j][t];
 				}
 			}
+
 		}
 		return ans;
+	}
+
+	private static void printMap(int[][] map)
+	{
+		for (int x = 1; x <= vertexCnt; x++) {
+			for (int y = 1; y <= vertexCnt; y++) {
+				System.out.print(map[x][y] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
